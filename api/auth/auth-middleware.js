b2c53f48dda6779ef32ac338af133cacb68ffd36
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-03-23 09:56:09 -0500

    model/middlware/secrets progress

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,67 +1,91 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { findBy } = require("../users/users-model");
 
+/*
+  If the user does not provide a token in the Authorization header:
+  status 401
+  {
+    "message": "Token required"
+  }
+
+  If the provided token does not verify:
+  status 401
+  {
+    "message": "Token invalid"
+  }
+
+  Put the decoded token in the req object, to make life easier for middlewares downstream!
+*/
 const restricted = (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header:
-    status 401
-    {
-      "message": "Token required"
-    }
-
-    If the provided token does not verify:
-    status 401
-    {
-      "message": "Token invalid"
-    }
-
-    Put the decoded token in the req object, to make life easier for middlewares downstream!
-  */
 }
 
+/*
+  If the user does not provide a token in the Authorization header with a role_name
+  inside its payload matching the role_name passed to this function as its argument:
+  status 403
+  {
+    "message": "This is not for you"
+  }
+
+  Pull the decoded token from the req object, to avoid verifying it again!
+*/
 const only = role_name => (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header with a role_name
-    inside its payload matching the role_name passed to this function as its argument:
-    status 403
-    {
-      "message": "This is not for you"
-    }
-
-    Pull the decoded token from the req object, to avoid verifying it again!
-  */
+  if(!req.token.role_name || req.token.role_name !== role_name) {
+    res.status(403).json({"message": "This is not for you"})
+  } else {
+    next()
+  }
 }
 
 
-const checkUsernameExists = (req, res, next) => {
-  /*
-    If the username in req.body does NOT exist in the database
-    status 401
-    {
-      "message": "Invalid credentials"
-    }
-  */
+/*
+  If the username in req.body does NOT exist in the database
+  status 401
+  {
+    "message": "Invalid credentials"
+  }
+*/
+const checkUsernameExists = async (req, res, next) => {
+  const userExists = await findBy({username: req.body.username})
+
+  if(!userExists) {
+    res.status(401).json({"message": "Invalid credentials"})
+  } else {
+    req.userExists = userExists
+    next()
+  }
 }
 
 
+/*
+  If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
+
+  If role_name is missing from req.body, or if after trimming it is just an empty string,
+  set req.role_name to be 'student' and allow the request to proceed.
+
+  If role_name is 'admin' after trimming the string:
+  status 422
+  {
+    "message": "Role name can not be admin"
+  }
+
+  If role_name is over 32 characters after trimming the string:
+  status 422
+  {
+    "message": "Role name can not be longer than 32 chars"
+  }
+*/
 const validateRoleName = (req, res, next) => {
-  /*
-    If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
+  const role = req.body.role_name.trim()
 
-    If role_name is missing from req.body, or if after trimming it is just an empty string,
-    set req.role_name to be 'student' and allow the request to proceed.
-
-    If role_name is 'admin' after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be admin"
-    }
-
-    If role_name is over 32 characters after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be longer than 32 chars"
-    }
-  */
+  if(!role || role.length < 1) {
+    req.role_name = 'student'
+    next()
+  } else if(role === 'admin') {
+    res.status(422).json({"message": "Role name can not be admin"})
+  } else if(role.length > 32) {
+    res.status(422).json({"message": "Role name can not be longer than 32 chars"})
+  }
 }
 
 module.exports = {
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,52 +1,67 @@
 const db = require('../../data/db-config.js');
 
-function find() {
-  /**
-    You will need to join two tables.
-    Resolves to an ARRAY with all users.
+/**
+  You will need to join two tables.
+  Resolves to an ARRAY with all users.
 
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "role_name": "admin"
-      },
-      {
-        "user_id": 2,
-        "username": "sue",
-        "role_name": "instructor"
-      }
-    ]
-   */
-}
-
-function findBy(filter) {
-  /**
-    You will need to join two tables.
-    Resolves to an ARRAY with all users that match the filter condition.
-
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "password": "$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq",
-        "role_name": "admin",
-      }
-    ]
-   */
-}
-
-function findById(user_id) {
-  /**
-    You will need to join two tables.
-    Resolves to the user with the given user_id.
-
+  [
+    {
+      "user_id": 1,
+      "username": "bob",
+      "role_name": "admin"
+    },
     {
       "user_id": 2,
       "username": "sue",
       "role_name": "instructor"
     }
-   */
+  ]
+ */
+function find() {
+  return db('users as u')
+  .join('roles as r', 'u.role_id', 'r.role_id')
+  .select('u.user_id', 'u.username', 'r.role_name')
+  .orderBy('user_id')
+}
+
+/**
+  You will need to join two tables.
+  Resolves to an ARRAY with all users that match the filter condition.
+
+  [
+    {
+      "user_id": 1,
+      "username": "bob",
+      "password": "$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq",
+      "role_name": "admin",
+    }
+  ]
+ */
+function findBy(filter) {
+  return ('users as u')
+  .join('roles as r', 'u.role_id', 'r.role_id')
+  .select('u.user_id', 'u.username', 'u.password', 'r.role_name')
+  .where(filter)
+  .first()
+  .orderBy('user_id')
+}
+
+/**
+  You will need to join two tables.
+  Resolves to the user with the given user_id.
+
+  {
+    "user_id": 2,
+    "username": "sue",
+    "role_name": "instructor"
+  }
+ */
+function findById(user_id) {
+  return db('users as u')
+  .join('roles as r', 'u.role_id', 'r.role_id')
+  .select('u.user_id', 'u.username', 'r.role_name')
+  .where({ user_id })
+  .first()
 }
 
 /**
